c04d37fcd529bbf0dd58d13d252d1006249c051e
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-08 14:12:45 -0500

    conditional to check if guest is right or left handed

--- src/scripts/guest/Guest.js
+++ src/scripts/guest/Guest.js
@@ -1,5 +1,6 @@
 export const Guest = (guest) => {
-    return `
+    if (guest.rightHanded) {
+        return `
     <div class="card">
         <img src="${guest.imageUrl}" style="width: 10rem; height: 10rem" class="card-img-top" alt="${guest.name}">
         <div class="card-body">
@@ -8,8 +9,24 @@ export const Guest = (guest) => {
         <ul class="list-group list-group-flush">
             <li class="list-group-item">${guest.age} yrs old</li>
             <li class="list-group-item">Favorite Dish: ${guest.favoriteDish}</li>
-            <li class="list-group-item">Right Handed? ${guest.rightHanded}</li>
+            <li class="list-group-item">Right Handed</li>
         </ul>
     </div>
-    `
+    `    
+    } else {
+        return `
+        <div class="card">
+            <img src="${guest.imageUrl}" style="width: 10rem; height: 10rem" class="card-img-top" alt="${guest.name}">
+            <div class="card-body">
+                <h5 class="card-title">${guest.name}</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${guest.age} yrs old</li>
+                <li class="list-group-item">Favorite Dish: ${guest.favoriteDish}</li>
+                <li class="list-group-item">Left Handed</li>
+            </ul>
+        </div>
+        `    
+    }
+    
 }
