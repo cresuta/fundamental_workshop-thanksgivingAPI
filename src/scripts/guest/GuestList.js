658f313e92a7cf7595f3db52d521f265497cc8f6
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-08 10:30:19 -0500

    guest list component created

---
+++ src/scripts/guest/GuestList.js
@@ -0,0 +1,20 @@
+import { getGuests, useGuests } from "../form/FormDataProvider.js"
+import { Guest } from "../guest/Guest.js";
+
+
+const guestList = document.querySelector('list-container')
+
+export const GuestList = () => {
+    getGuests()
+    .then(() => {
+        let guests = useGuests();
+        let guestListHTML = "";
+        guests.forEach((singleGuest) => {
+            guestListHTML += Guest(singleGuest);
+        })
+    })
+
+    guestList.innerHTML = `
+        ${guestListHTML}
+    `
+}
