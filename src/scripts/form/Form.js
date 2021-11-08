d9bc27b39354f9eabfe8b6cd101c3b3bf58ab235
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-08 10:50:43 -0500

    event listener added to submit btn

--- src/scripts/form/Form.js
+++ src/scripts/form/Form.js
@@ -1,5 +1,30 @@
+import {saveGuest} from "../form/FormDataProvider.js"
+import { GuestList } from "../guest/GuestList.js";
 
 const guestForm = document.querySelector('.form-container');
+
+document.querySelector("body").addEventListener("click", clickEvent => {
+  if(clickEvent.target.id = "submit") {
+    
+    const newGuest = {
+      name: document.querySelector("#name").value,
+      age: document.querySelector("#age").value,
+      favoriteDish: document.querySelector("#favoriteDish").value,
+      rightHanded: document.querySelector("#isRightHanded").value,
+      imageUrl: document.querySelector("#imageUrl").value
+    }
+
+      document.querySelector("#name").value = ""
+      document.querySelector("#age").value = ""
+      document.querySelector("#favoriteDish").value = ""
+      document.querySelector("#isRightHanded").value = ""
+      document.querySelector("#imageUrl").value = ""
+
+      saveGuest(newGuest)
+      .then(GuestList)     
+
+  }
+})
 
 
 export const Form = () => {
@@ -7,32 +32,29 @@ export const Form = () => {
     <form>
     <div class="form-group">
       <label for="name">Name</label>
-      <input type="text" class="form-control" id="" placeholder="Enter full name">
+      <input type="text" class="form-control" id="name" placeholder="Enter full name">
     </div>
     <div class="form-group">
       <label for="age">Age</label>
-      <input type="text" class="form-control" id="" placeholder="Enter age">
+      <input type="text" class="form-control" id="age" placeholder="Enter age">
     </div>
-    <select class="form-select" aria-label="Default select example">
-      <option selected>Favorite Dish</option>
+    <select class="form-select" aria-label="Default select example" id="favoriteDish">
+      <option selected>Favorite Dish?</option>
       <option value="sweet-potatoes">Sweet Potatoes</option>
       <option value="shaken-dirties">Shaken Dirties</option>
       <option value="stuffing">Stuffing</option>
       <option value="pumpkin-pie">Pumpkin Pie</option>
     </select>
-    <div class="form-check form-check-inline">
-      <input class="form-check-input" type="radio" name="" id="" value="left-handed">
-      <label class="form-check-label" for="left-handed">Left-Handed</label>
-    </div>
-    <div class="form-check form-check-inline">
-     <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="right-handed">
-     <label class="form-check-label" for="right-handed">Right-Handed</label>
-    </div>
+    <select class="form-select" aria-label="Default select example" id="isRightHanded">
+      <option selected>Are you right handed?</option>
+      <option value="true">Yes</option>
+      <option value="false">No</option>
+    </select>
     <div class="form-group">
       <label for="image">Your Image</label>
-      <input type="file" class="form-control-file" id="">
+      <input type="file" class="form-control-file" id="imageUrl">
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary" id="submit">Submit</button>
     </form>
     `
 }
